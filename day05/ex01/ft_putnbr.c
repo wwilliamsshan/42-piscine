a5a290846da6f323fd0b883926d892ee520340da
wshan <wwilliamsshan@gmail.com>
2018-05-21 13:53:46 -0700

    accounting for negative min

--- day05/ex01/ft_putnbr.c
+++ day05/ex01/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: wshan <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/21 12:58:41 by wshan             #+#    #+#             */
-/*   Updated: 2018/05/21 13:40:10 by wshan            ###   ########.fr       */
+/*   Updated: 2018/05/21 13:53:30 by wshan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,15 @@ void	ft_putchar(char c)
 
 void	ft_putnbr(int nb)
 {
-	if (nb <= -10)
+	if (nb == -2147483648)
+	{
+		ft_putnbr(-214748364);
+		ft_putchar(8 + 48);
+	}
+	else if (nb <= -10)
 	{
 		ft_putchar('-');
-		ft_putnbr((-1 * nb)/10);
+		ft_putnbr((-1 * nb) / 10);
 		ft_putchar(((-1 * nb) % 10) + 48);
 	}
 	else if (-10 < nb && nb < 0)
@@ -30,13 +35,13 @@ void	ft_putnbr(int nb)
 		ft_putchar('-');
 		ft_putchar((-1 * nb) + 48);
 	}
-	else if (0 <= nb &&  nb < 10)
+	else if (0 <= nb && nb < 10)
 	{
 		ft_putchar(nb + 48);
 	}
 	else if (10 <= nb)
 	{
-		ft_putnbr(nb/10);
+		ft_putnbr(nb / 10);
 		ft_putchar((nb % 10) + 48);
 	}
 }
