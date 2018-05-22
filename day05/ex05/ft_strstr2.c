fcf86d672a263ef4aa2a624d41372ae147fb1fd2
wshan <wwilliamsshan@gmail.com>
2018-05-21 21:31:05 -0700

    initial commit

---
+++ day05/ex05/ft_strstr2.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wshan <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/21 19:48:34 by wshan             #+#    #+#             */
+/*   Updated: 2018/05/21 21:30:05 by wshan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int a;
+	int b;
+	int length;
+
+	length = 0;
+	while (to_find[length] != '\0')
+	{
+		length = length + 1;
+	}
+	if (length == 0)
+		return (str);
+	a = 0;
+	b = 0;
+	while (str[a] != '\0')
+	{
+		while (str[a + b] == to_find[b])
+		{
+			if (b == length - 1)
+				return (str + a);
+			b = b + 1;
+		}
+		a = a + 1;
+	}
+	return (0);
+}
