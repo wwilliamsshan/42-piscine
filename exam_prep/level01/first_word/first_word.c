266ec78a3caf9052f99aa411e839ffdfa006e267
wshan <wwilliamsshan@gmail.com>
2018-06-01 14:21:19 -0700

    first word

---
+++ exam_prep/level01/first_word/first_word.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   first_word.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wshan <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/01 14:13:36 by wshan             #+#    #+#             */
+/*   Updated: 2018/06/01 14:20:51 by wshan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	first_word(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != ' ' && str[i] != '\t')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+	ft_putchar('\n');
+}
+
+int		main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		ft_putchar('\n');
+	}
+	else
+	{
+		first_word(argv[1]);
+	}
+	return (0);
+}
