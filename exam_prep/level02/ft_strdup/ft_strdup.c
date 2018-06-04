6137d57b0a7fcab5d53fccb680c7f97fb414cbe2
wshan <wwilliamsshan@gmail.com>
2018-06-04 11:08:43 -0700

    ft_strdup

---
+++ exam_prep/level02/ft_strdup/ft_strdup.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wshan <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/04 10:54:53 by wshan             #+#    #+#             */
+/*   Updated: 2018/06/04 11:08:32 by wshan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+char	*ft_strdup(char *src)
+{
+	int		i;
+	char	*copy;
+	int		length;
+	
+	length = 0;
+	while (src[length] != '\0')
+	{
+		length++;
+	}
+	copy = (char *)malloc(sizeof(char) * length + 1);
+	while (src[i] != '\0')
+	{
+		copy[i] = src[i];
+		i++;
+	}
+	copy[i] = '\0';
+	return (copy);
+}
+
+int		main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		return (0);
+	}
+	else
+	{
+		ft_putstr(ft_strdup(argv[1]));
+	}
+	return (0);
+}
