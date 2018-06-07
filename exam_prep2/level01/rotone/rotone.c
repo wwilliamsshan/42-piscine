2db21a9fa8fdcdcaf5609225ee23ed258b46e064
wshan <wwilliamsshan@gmail.com>
2018-06-06 19:44:26 -0700

    rotone

---
+++ exam_prep2/level01/rotone/rotone.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rotone.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wshan <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/06 19:39:11 by wshan             #+#    #+#             */
+/*   Updated: 2018/06/06 19:44:18 by wshan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	rotone(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if ((str[i] >= 'a' && str[i] <= 'y') || (str[i] >= 'A' && str[i] <= 'Y'))
+		{
+			ft_putchar(str[i] + 1);
+		}
+		else if (str[i] == 'z' || str[i] == 'Z')
+		{
+			ft_putchar(str[i] - 25);
+		}
+		else
+		{
+			ft_putchar(str[i]);
+		}
+		i++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		rotone(argv[1]);
+	}
+	ft_putchar('\n');
+	return (0);
+}
