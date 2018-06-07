ffe40f97891dcb3385f9f003d407662e1218e2a4
wshan <wwilliamsshan@gmail.com>
2018-06-06 23:24:30 -0700

    all

--- exam_prep2/level02/is_power_of_2/is_power_of_2.c
+++ exam_prep2/level02/is_power_of_2/is_power_of_2.c
@@ -6,8 +6,34 @@
 /*   By: wshan <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/06 22:59:37 by wshan             #+#    #+#             */
-/*   Updated: 2018/06/06 23:05:14 by wshan            ###   ########.fr       */
+/*   Updated: 2018/06/06 23:24:25 by wshan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 int		is_power_of_2(unsigned int n)
+{
+	if (n == 0)
+	{
+		return (0);
+	}
+	while (n % 2 == 0)
+	{
+		n = n / 2;
+	}
+	if (n == 1)
+	{
+		return (1);
+	}
+	return (0);
+}
+
+int		main(void)
+{
+	unsigned int a;
+
+	a = 17;
+	printf("%d", is_power_of_2(a));
+	return (0);
+}
